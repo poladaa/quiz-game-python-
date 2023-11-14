8be1470227b7f2d8c2fc000609eea7b170ab28c9
luka <120731202+poladaa@users.noreply.github.com>
2023-11-14 19:29:50 +0400

    Add files via upload

---
+++ quiz game/main.py
@@ -0,0 +1,79 @@
+#
+def new_game():
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
+
+    for key in questions:
+        print('-------------')
+        print(key)
+        for i in options[question_num - 1]:
+            print(i)
+        guess = input('enter (A,B,C, or D)').upper()
+        guesses.append(guess)
+
+        correct_guesses +=   check_answer(questions.get(key), guess)
+        question_num += 1
+
+    display_score(correct_guesses, guesses)
+#
+def check_answer(answer, guess):
+    if answer == guess:
+        print('Correct!')
+        return 1
+    else:
+        print('Wrong')
+        return 0
+
+#
+def display_score(correct_guesses, guesses):
+    print('-------------------')
+    print('Results')
+    print('-------------------')
+    print('Answers: ', end='')
+    for i in questions:
+        print(questions.get(i), end=' ')
+    print()
+
+    print('Guesses: ', end='')
+    for i in guesses:
+        print(i, end=' ')
+    print()
+
+    score = int(correct_guesses/len(questions)*100)
+    print('Your score is ' + str(score) + '%')
+#
+def play_again():
+    response = input('Do you want to pl;ay again?: (YES OR NO)').upper()
+
+    if response == 'YES':
+         return True
+    else:
+        return False
+
+
+
+
+
+
+
+questions = {
+    'who created python? ': 'A',
+    'What year was Python created?: ': 'B',
+    'Python is tributed to which comedy group?: ': 'C',
+    'Is the earth round? ': 'A'
+}
+
+
+options = [['A. Guido van Rossum', 'B. Elon Musk', 'C. Bill Gates', 'D. Mark zUCKERBURG'],
+           ['A. 1989', 'B. 1991', 'C. 2000', 'D. 2016'],
+           ['A. lONELY iSLAND', 'B. Smosh', 'C. Monty Python', 'D. SNL'],
+           ['A. True', 'B. false', 'C. sometimes', 'D. What is Earth?']
+           ]
+
+new_game()
+
+while play_again():
+    new_game()
+
+print('byeeee')
